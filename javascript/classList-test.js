17b3e4d84089c6b6a1b9f8e62d965dab9d72be18
changsubkwak <paradise1732@gmail.com>
2023-01-05 21:16:11 +0900

    feat: study usages of classList

---
+++ javascript/classList-test.js
@@ -0,0 +1,28 @@
+const div = document.createElement("div");
+div.className = "foo";
+
+console.log(div.outerHTML);
+
+div.classList.remove("foo");
+div.classList.add("anotherclass");
+
+console.log(div.outerHTML);
+
+div.classList.toggle("visible");
+console.log(div.outerHTML);
+
+let i = 9;
+div.classList.toggle("visible", i < 10);
+console.log(div.outerHTML);
+
+i = 11;
+div.classList.toggle("visible", i < 10);
+console.log(div.outerHTML);
+
+console.log(div.classList.contains("foo"));
+
+div.classList.add("foo", "bar", "baz");
+console.log(div.outerHTML);
+
+div.classList.remove("foo", "bar", "baz");
+console.log(div.outerHTML);
